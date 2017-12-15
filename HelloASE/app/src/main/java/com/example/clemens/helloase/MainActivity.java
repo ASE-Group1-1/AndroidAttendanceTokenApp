5f4c1652dcdf9d2c363c95f717d4cc8050c33c72
clemenszuck <clemens@zuck-online.de>
2017-12-15 14:33:18 +0100

    First version of the App

---
+++ HelloASE/app/src/main/java/com/example/clemens/helloase/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.clemens.helloase;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import org.restlet.resource.ClientResource;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_qr) {
+            TextView main_textview = (TextView)findViewById(R.id.textview_content);
+
+            main_textview.setText("Show QR code");
+        }
+        if (id == R.id.action_download) {
+            TextView main_textview = (TextView) findViewById(R.id.textview_content);
+            String url = "http://ase2016-148507.appspot.com/rest/guestbook/";
+            System.out.println("1");
+            try {
+                System.out.println("2");
+                String rawAnswer = new ClientResource(url).get().getText();
+                System.out.println("3");
+                main_textview.setText(rawAnswer);
+                System.out.println("4");
+                }
+            catch(Exception exc) {
+                main_textview.setText(exc.getMessage());
+            }
+            System.out.println("5");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
